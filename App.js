7f2eeac33131838e29f262d41e42c98fdd511d02
pierrooTH <pierre.thouvenot1711@gmail.com>
2023-07-24 10:57:59 +0200

    feat(signupForm): creation signup form

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import signupForm from './signupForm';
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen  name="Home" component={signupForm} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ signupForm.js
@@ -0,0 +1,53 @@
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+
+export default function signupForm() {
+    const [email, setEmail] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+
+  return (
+    <View style={{marginTop: 50, padding: 20}}>
+
+      <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 40}}>Paye ton Kawa!</Text>
+      <View style={{marginTop: 10, display: 'flex', flexDirection: 'column'}}>
+        <View style={{display: 'flex', flexDirection: 'row', gap: '10px', width: '100%'}}>
+            <View style={{ width: '49%'}}>
+                <Text style={{marginBottom: 10}}>Prénom</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder={"Ex. John"}
+                    onChangeText={(text) => setFirstname(text)}
+                />
+            </View>
+            <View style={{ width: '49%'}}>
+            <Text style={{marginBottom: 10}}>Nom</Text>
+            <TextInput
+                style={styles.input}
+                placeholder={"Ex. Doe"}   
+                onChangeText={(text) => setLastname(text)}
+            />
+            </View>
+        </View>  
+            <Text style={{marginBottom: 10}}>Email</Text>
+            <TextInput
+                style={styles.input}
+                placeholder={"Ex. john.doe@gmail.com"}
+                onChangeText={(text) => setEmail(text)}
+                autoCapitalize={"none"}
+            />
+      </View>
+      <Button title={"Sign Up"} onPress={() => {}} />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      marginBottom: 10,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      padding: 5
+    },
+  });
