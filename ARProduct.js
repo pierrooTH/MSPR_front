d783664fa5da6b13750df57addf96d7679d2bec0
pierrooTH <pierre.thouvenot1711@gmail.com>
2023-07-25 10:50:56 +0200

    feat(qrCode): added qrcode scan page

--- ARProduct.js
+++ ARProduct.js
@@ -1,47 +1,101 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
-import ExpoTHREE, { AR } from 'expo-three';
-import * as ThreeAR from 'expo-three-ar';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import { resolveAsync } from 'expo-asset-utils';
+import * as FileSystem from 'expo-file-system';
+import { decode } from 'base64-arraybuffer';
+import { Renderer, TextureLoader, loadObjAsync, loadTextureAsync } from 'expo-three';
+import OrbitControlsView from 'expo-three-orbit-controls';
+import { Asset } from 'expo-asset'
+import {
+  AmbientLight,
+  BoxGeometry,
+  Fog,
+  GridHelper,
+  Mesh,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  PointLight,
+  Scene,
+  SpotLight,
+  Camera,
+} from 'three';
 
-export default function ARProduct() {
-  const renderer = useRef(null);
+const ARProduct = () => {
+  const [camera, setCamera] = useState(null);
+
+  let timeout;
 
   useEffect(() => {
-    (async () => {
-      await ThreeAR.isAvailableAsync();
-      AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
-
-      renderer.current = new ExpoTHREE.Renderer();
-      renderer.current.setSize(window.innerWidth, window.innerHeight);
-
-      AR.onFrameDidUpdate(({ camera }) => {
-        renderer.current.render(AR.scene, camera);
-      });
-    })();
+    return () => clearTimeout(timeout);
   }, []);
 
+  const onContextCreate = async (gl) => {
+    const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+    const sceneColor = 0x6ad6f0;
+
+    // Create a WebGLRenderer without a DOM element
+    const renderer = new Renderer({ gl });
+    renderer.setSize(width, height);
+    renderer.setClearColor(sceneColor);
+
+    const camera = new PerspectiveCamera(70, width / height, 0.01, 1000);
+    camera.position.set(2, 5, 5);
+    setCamera(camera);
+
+    const scene = new Scene();
+    scene.fog = new Fog(sceneColor, 1, 10000);
+    scene.add(new GridHelper(10, 10));
+
+    const ambientLight = new AmbientLight(0xB1B1B1, 1.5);
+    scene.add(ambientLight);
+
+    const spotLight = new SpotLight(0xffffff, 1.5);
+    spotLight.position.set(0, 200, 200);
+    spotLight.lookAt(scene.position);
+    scene.add(spotLight);
+
+    // const texture = await loadTextureAsync({
+    //   asset: require('./assets/machineend.mtl'),
+    // });
+
+    // const obj = await loadObjAsync({
+    //   asset: require('./assets/machineend.obj')
+    // });
+    obj.traverse(function(object) {
+      if (object instanceof THREE.Mesh) {
+        object.material.map = texture;
+      }
+    });
+
+    scene.add(obj);
+
+    camera.lookAt(obj.position);
+
+    function update() {
+      obj.rotation.y = 0;
+      obj.rotation.x = 0;
+    }
+
+    // Setup an animation loop
+    const render = () => {
+      timeout = requestAnimationFrame(render);
+      update();
+      renderer.render(scene, camera);
+
+      gl.endFrameEXP();
+    };
+    render();
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <ExpoTHREE.View
-        style={{ flex: 1 }}
-        onContextCreate={async (gl) => {
-          AR.scene = new THREE.Scene();
-          AR.camera = ExpoTHREE.createARCamera(
-            gl,
-            window.innerWidth,
-            window.innerHeight,
-            0.01,
-            1000
-          );
-
-          const modelUri = require('./3DModels/machineend.glb'); // Replace 'your_model.gltf' with your actual model file
-
-          const model = await ExpoTHREE.loadAsync(modelUri);
-          model.position.set(0, 0, -1); // Set the initial position of the model
-
-          AR.scene.add(model);
-        }}
-      />
+    <View style={{flex: 1}}>
+      <OrbitControlsView style={{ flex: 1 }} camera={camera}>
+        <GLView style={{ flex: 1 }} onContextCreate={onContextCreate} key="d" />
+      </OrbitControlsView>
     </View>
+
   );
-}
+};
+
+export default ARProduct;
--- App.js
+++ App.js
@@ -5,14 +5,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignupForm from './signupForm';
 import ProductScreen from './productScreen';
 import ArProduct from './ARProduct';
+import QrCode from './components/qrCode';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName="AR">
+       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen  name="Home" component={SignupForm} options={{headerShown: false}} />
         <Stack.Screen  name="Product" component={ProductScreen} options={{title: 'Produits'}}/>
-        <Stack.Screen  name="AR" component={ArProduct} options={{headerShown: false}}/>
+        <Stack.Screen  name="QrCode" component={QrCode} options={{headerShown: false}}/>
+        {/* <Stack.Screen  name="AR" component={ArProduct} options={{headerShown: false}}/> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
