6a4fe066a250216ae95e05d40439deae5c1586c2
Paul LEBLOND <paul.leblond@yoozly.com>
2023-08-27 17:57:16 +0200

    fix(lint) : fix error in app.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignupForm from './signupForm';
 import ProductScreen from './productScreen';
 import QrCode from './components/qrCode';
@@ -9,23 +9,49 @@ import ARProduct from './components/ARProduct';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-    
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName='Product'>
-        <Stack.Screen  name="Home" component={SignupForm} options={{headerShown: false}} />
-        <Stack.Screen  name="QrCode" component={QrCode} options={{headerShown: false}}/>
-        <Stack.Screen  name="Product" component={ProductScreen} options={{title: 'Liste des articles', headerStyle: {backgroundColor: '#5C4F2D'}, headerTitleStyle: {
-            color: 'white',
-          },}}/>
-        <Stack.Screen  name="Details" component={ProductDetails} options={{title: 'Informations du produit', headerStyle: {backgroundColor: '#5C4F2D'}, headerTitleStyle: {
-            color: 'white',
-          },}}/>
-        <Stack.Screen  name="AR" component={ARProduct} options={{headerShown: false}}/>
+      <Stack.Navigator initialRouteName="Product">
+        <Stack.Screen
+          name="Home"
+          component={SignupForm}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="QrCode"
+          component={QrCode}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Product"
+          component={ProductScreen}
+          options={{
+            title: 'Liste des articles',
+            headerStyle: {backgroundColor: '#5C4F2D'},
+            headerTitleStyle: {
+              color: 'white',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={ProductDetails}
+          options={{
+            title: 'Informations du produit',
+            headerStyle: {backgroundColor: '#5C4F2D'},
+            headerTitleStyle: {
+              color: 'white',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="AR"
+          component={ARProduct}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
